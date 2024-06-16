d3ab3f66355893fbccb74ee62dfd161d2596ca09
MaxZiCode <maxzicode@gmail.com>
2024-06-16 14:23:13 +0300

    Update for game version 1.5.

--- Source/TranslationInfo.cs
+++ Source/TranslationInfo.cs
@@ -11,60 +11,60 @@ using Verse;
 
 namespace TranslatedNames
 {
-	public static class TranslationInfo
-	{
-		private static readonly Dictionary<string, string> translations = new Dictionary<string, string>();
+    public static class TranslationInfo
+    {
+        private static readonly Dictionary<string, string> translations = new Dictionary<string, string>();
 
-		static TranslationInfo()
-		{
-			foreach (var filePath in Directory.GetFiles(StaticConstructor.translationPath))
-				try
-				{
-					XDocument xd = XDocument.Load(filePath);
+        static TranslationInfo()
+        {
+            foreach (var filePath in Directory.GetFiles(StaticConstructor.translationPath))
+                try
+                {
+                    XDocument xd = XDocument.Load(filePath);
 
-					foreach (var xe in xd.Root.Descendants())
-					{
-						try
-						{
-							string key = (string)xe.Attribute("name");
-							string value = (string)xe.Attribute("t-Name");
-							if (!translations.ContainsKey(key))
-								translations.Add(key, value);
-						}
-						catch (Exception ex)
-						{
-							Log.Error(ex.ToString());
-						}
-					}
-				}
-				catch (Exception ex)
-				{
-					Log.Error(ex.ToString());
-				}
-		}
+                    foreach (var xe in xd.Root.Descendants())
+                    {
+                        try
+                        {
+                            string key = (string)xe.Attribute("name");
+                            string value = (string)xe.Attribute("t-Name");
+                            if (!translations.ContainsKey(key))
+                                translations.Add(key, value);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex.ToString());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.ToString());
+                }
+        }
 
-		public static string GetTranslation(string name)
-		{
-			string tName = name;
-			string engPattern = "[A-Za-z]+";
+        public static string GetTranslation(string name)
+        {
+            string tName = name;
+            string engPattern = "[A-Za-z]+";
 
-			if (Regex.IsMatch(name, engPattern))
-			{
-				if (translations.ContainsKey(name))
-				{
-					tName = translations[name];
+            if (Regex.IsMatch(name, engPattern))
+            {
+                if (translations.ContainsKey(name))
+                {
+                    tName = translations[name];
 
 #if DEBUG
-					if (Regex.IsMatch(tName, engPattern))
-						Log.Warning($"The name '{tName}' not translated in the translation dictionary!"); 
+                    if (Regex.IsMatch(tName, engPattern))
+                        Log.Warning($"The name '{tName}' not translated in the translation dictionary!");
 #endif
-				}
+                }
 #if DEBUG
-				else
-					Log.Warning($"The translation dictionary doesn't contain the key '{tName}'."); 
+                else
+                    Log.Warning($"The translation dictionary doesn't contain the key '{tName}'.");
 #endif
-			}
-			return tName;
-		}
-	}
+            }
+            return tName;
+        }
+    }
 }
